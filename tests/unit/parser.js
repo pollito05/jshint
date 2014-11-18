78fa8be0bc6e913eb9278cca011b96edb8de6e7c
Mike Pennisi <mike@mikepennisi.com>
2014-11-18 15:10:12 -0500

    Add a regression test

--- tests/unit/parser.js
+++ tests/unit/parser.js
@@ -3728,6 +3728,13 @@ exports["fat arrow nested function scoping"] = function (test) {
   test.done();
 };
 
+exports["default arguments in fat arrow functions"] = function (test) {
+  TestRun(test)
+    .test("(x = 0) => { return x; };", { expr: true, unused: true, esnext: true });
+
+  test.done();
+};
+
 exports["concise methods support"] = function (test) {
   var code = [
     "var foobar = {",
