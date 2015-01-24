5ef72a0569d8e8dd5905a339c65e62a968251d56
Mike Pennisi <mike@mikepennisi.com>
2015-01-24 01:07:07 -0500

    Simplify

--- tests/browser.js
+++ tests/browser.js
@@ -4,28 +4,18 @@ var phantom = require("phantom");
 var createTestServer = require("./helpers/browser/server");
 var port = 8045;
 
-phantom.create(function (ph) {
-  ph.createPage(function (page) {
+phantom.create(function(ph) {
+  ph.createPage(function(page) {
     createTestServer(port, function(server) {
-      var failures = [];
-
       page.onConsoleMessage(function(str) {
-        var parts = str.split(":");
-        var failureCount = parseInt(parts[2], 10);
-
         console.log(str);
+      });
 
-        if (parts[0] === "all") {
-          ph.exit();
-          server.close();
-
-          if (failureCount > 0) {
-            failures.unshift("");
-            console.error(failures.join("\n  "));
-            process.exit(1);
-          }
-        } else if (failureCount > 0) {
-          failures.push(str);
+      page.set('onCallback', function(err) {
+        ph.exit();
+        server.close();
+        if (err) {
+          process.exit(1);
         }
       });
 
